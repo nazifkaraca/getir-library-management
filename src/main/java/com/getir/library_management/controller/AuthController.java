2e25eeec5271e2353a6960f530c8d1d6ff7d8b0d
Nazif Karaca <nazif808@gmail.com>
2025-04-29 20:16:29 +0300

    Getir Library Management v1.3: <create> Authentication operations seperated.

---
+++ src/main/java/com/getir/library_management/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.getir.library_management.controller;
+
+import com.getir.library_management.dto.Auth.AuthenticationRequestDto;
+import com.getir.library_management.dto.Auth.AuthenticationResponseDto;
+import com.getir.library_management.dto.User.RegisterRequestDto;
+import com.getir.library_management.dto.User.UserResponseDto;
+import com.getir.library_management.service.impl.AuthServiceImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthServiceImpl authService;
+
+    // Register endpoint
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> register(@Valid @RequestBody RegisterRequestDto request) {
+        return new ResponseEntity<>(authService.register(request), HttpStatus.CREATED);
+    }
+
+    // Login endpoint
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponseDto> login(@Valid @RequestBody AuthenticationRequestDto request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
---
+++ src/main/java/com/getir/library_management/service/impl/AuthServiceImpl.java
@@ -0,0 +1,69 @@
+package com.getir.library_management.service.impl;
+
+import com.getir.library_management.dto.Auth.AuthenticationRequestDto;
+import com.getir.library_management.dto.Auth.AuthenticationResponseDto;
+import com.getir.library_management.dto.User.RegisterRequestDto;
+import com.getir.library_management.dto.User.UserResponseDto;
+import com.getir.library_management.entity.Role;
+import com.getir.library_management.entity.User;
+import com.getir.library_management.exception.custom.EmailAlreadyExistsException;
+import com.getir.library_management.exception.custom.UserNotFoundException;
+import com.getir.library_management.repository.UserRepository;
+import com.getir.library_management.service.AuthService;
+import com.getir.library_management.util.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final ModelMapper modelMapper;
+    private static final String USER_NOT_FOUND = "User not found.";
+
+    // Register
+    @Override
+    public UserResponseDto register(RegisterRequestDto request) {
+        // Check if the email is already registered
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new EmailAlreadyExistsException("Email is already registered: " + request.getEmail());
+        }
+
+        // Create new user
+        User newUser = User.builder()
+                .fullName(request.getFullName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword())) // Encrypt password with BCrypt
+                .role(Role.ROLE_USER) // Assign default role of "User"
+                .build();
+
+        // Save new user
+        User savedUser = userRepository.save(newUser);
+
+        // Return user response DTO
+        return modelMapper.map(savedUser, UserResponseDto.class);
+    }
+
+    // Login
+    @Override
+    public AuthenticationResponseDto login(AuthenticationRequestDto request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid credentials.");
+        }
+
+        String token = jwtService.generateToken(user);
+
+        return AuthenticationResponseDto.builder()
+                .token(token)
+                .build();
+    }
+}
