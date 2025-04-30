a95996df4fc24a72164791f76fed5af10a4f70de
Nazif Karaca <nazif808@gmail.com>
2025-04-30 20:48:06 +0300

    Getir Library Management v2.1: <create> Error messages centralized in a class.

---
+++ src/main/java/com/getir/library_management/exception/ErrorMessages.java
@@ -0,0 +1,7 @@
+package com.getir.library_management.exception;
+
+public class ErrorMessages {
+    public static final String BOOK_NOT_FOUND = "Book not found.";
+    public static final String BOOK_EXISTS = "Book already exists.";
+    public static final String USER_NOT_FOUND = "User not found.";
+}
